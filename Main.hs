03bd27d849ec451455cfb98d1163c595b8dba6b0
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-08-16 17:49:46 -0400

    WordBank is done

--- Main.hs
+++ Main.hs
@@ -4,5 +4,9 @@ import WordBank.WordBank (createWordBank)
 main :: IO ()
 main = do
     args <- getArgs
-    content <- createWordBank
-    putStr content
+    wordBank <- createWordBank
+    case head args of
+        "MakeWordBank" ->
+            writeFile "data/word_bank.txt" wordBank
+        _ ->
+            putStr "Error"
--- WordBank/WordBank.hs
+++ WordBank/WordBank.hs
@@ -1,4 +1,36 @@
 module WordBank.WordBank (createWordBank) where
+import Data.List (nub, sort)
+import Distribution.Compat.CharParsing (CharParsing(string))
+import Data.Char (toLower, isAscii, isLetter)
 
 createWordBank :: IO String
-createWordBank = readFile "./data/text.txt"
+createWordBank =
+    makeString .
+    makeNoDuplicateList .
+    readFile $ file
+
+file :: FilePath
+file = "data/text.txt"
+
+makeNoDuplicateList :: IO String -> IO [String]
+makeNoDuplicateList ioString = do
+    string <- ioString
+    return $
+        sort .
+        filter (not . null) .
+        nub .
+        map (filter isLetter . filter isAscii . map toLower) .
+        words $ string
+
+-- stupidCharacterFilter :: String -> Bool
+-- stupidCharacterFilter a = not (isLetter (head a) || isAscii (last a))
+
+makeString :: IO [String] -> IO String
+makeString l = do
+    listWordBank <- l
+    return $ listToString listWordBank ""
+
+listToString :: [String] -> String -> String
+listToString (x:xs) str = case x:xs of
+    [x] -> str ++ ", " ++ x
+    _ -> if str /= "" then listToString xs $ str ++ ", " ++ x else listToString xs $ str ++ x
