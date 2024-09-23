eb0f6ff137d13e68986f478c1aa7ec757ce30597
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-09-23 12:24:20 -0400

    (WARNING: NOT WORKING) Added hangmanGambit function

--- src/GuessTheWord/GuessWordGame.hs
+++ src/GuessTheWord/GuessWordGame.hs
@@ -14,13 +14,13 @@ import Data.List.Split ( splitOn )
 import GHC.Float ( int2Float )
 import System.Random (randomRIO)
 
-data GameSettings = GameSettings {lives :: Int, hiddenLettersPercent :: Float, difficulty :: Difficulty}
+data GameSettings = GameSettings {lives :: Int, hiddenLettersPercent :: Float, difficulty :: Difficulty, score :: Int}
 
 type GameState = StateT GameSettings IO
 
 guessWordGame:: Difficulty -> IO ()
 guessWordGame dif = do
-    gameSettings <- execStateT (setSettings dif) (GameSettings 0 0 Easy)
+    gameSettings <- execStateT (setSettings dif) (GameSettings 0 0 Easy 0)
     evalStateT gameIntro gameSettings
 
 gameIntro :: GameState ()
@@ -37,8 +37,28 @@ playGame :: GameState ()
 playGame = do
     gameState <- get
     wordToFind <- liftIO findRandomWord
-    processedWord <- liftIO $ processWord wordToFind (hiddenLettersPercent gameState)
-    liftIO $ putStrLn $ fst processedWord
+
+    let numHiddenChars = floor (hiddenLettersPercent gameState * int2Float (length wordToFind))
+    hiddenOutputs <- liftIO (hiddenOutput (length wordToFind) numHiddenChars)
+
+    let processedWord = processWord wordToFind hiddenOutputs
+
+    if gameState 
+    hangmanGambit processedWord
+
+hangmanGambit :: (String, String) -> IO ()
+hangmanGambit wordTuple = do
+    guess <- liftIO readLn
+
+    let pointsForWord = 10 * length (filter (== '_') (snd wordTuple))
+
+    if lives gameState == 0 
+        then gameOver
+    else
+        if guess == snd wordTuple then
+            increaseScore pointsForWord
+        else 
+            die >> hangmanGambit wordTuple
 
 wordBankList :: IO [String]
 wordBankList = splitOn ", " <$> readFile "data/word_bank.txt"
@@ -48,14 +68,11 @@ findRandomWord = do
     wordList <- wordBankList
     randomIndex <- randomRIO (0, length wordList)
     return $ wordList !! randomIndex
-    
-processWord :: String -> Float -> IO (String, String)
-processWord word hiddenLettersPercent = do
-    hiddenList <- hiddenOutput (length word) numHiddenChars
-    return $ (zipWith
-        (\char hiddenChar -> if hiddenChar == '0' then '_' else char)
-        word hiddenList, word)
-    where numHiddenChars = floor (hiddenLettersPercent * int2Float (length word))
+
+processWord :: String -> String -> (String, String)
+processWord word hiddenOutput =
+    (zipWith (\char hiddenChar -> if hiddenChar == '0' then '_' else char)
+        word hiddenOutput, word) -- tuple: (hiddenWord, originalWord)
 
 hiddenOutput :: Int -> Int -> IO String
 hiddenOutput wordLength numHiddenChars = do
@@ -66,14 +83,25 @@ hiddenOutput wordLength numHiddenChars = do
 
 setSettings :: Difficulty -> GameState()
 setSettings difficulty = case difficulty of
-        Easy -> put GameSettings { lives = 7, hiddenLettersPercent = 10 / 100, difficulty = Easy }
-        Normal -> put GameSettings { lives = 5, hiddenLettersPercent = 20 / 100, difficulty = Normal }
-        Hard -> put GameSettings { lives = 3, hiddenLettersPercent = 30 / 100, difficulty = Hard }
-        Hardcore -> put GameSettings { lives = 1, hiddenLettersPercent = 30 / 100, difficulty = Hardcore }
-        SigmaMale -> put GameSettings { lives = 1, hiddenLettersPercent = 70 / 100, difficulty = SigmaMale }
+        Easy -> put GameSettings { lives = 7, hiddenLettersPercent = 10 / 100, difficulty = Easy, score = 0}
+        Normal -> put GameSettings { lives = 5, hiddenLettersPercent = 20 / 100, difficulty = Normal, score = 0 }
+        Hard -> put GameSettings { lives = 3, hiddenLettersPercent = 30 / 100, difficulty = Hard, score = 0 }
+        Hardcore -> put GameSettings { lives = 1, hiddenLettersPercent = 30 / 100, difficulty = Hardcore, score = 0 }
+        SigmaMale -> put GameSettings { lives = 1, hiddenLettersPercent = 70 / 100, difficulty = SigmaMale, score = 10 }
 
 die :: GameState ()
 die = do
     currentSettings <- get
     let l = lives currentSettings
     modify (\gs -> gs { lives = l - 1 })
+
+increaseScore :: Int -> GameState()
+increaseScore = do
+    gameState <- get
+    let currentScore = score gameState
+    modify (\gs -> gs {score = currentScore + pointsForWord})
+
+gameOver :: GameState ()
+gameOver = do
+    gameState <- get
+    liftIO $ print ("Game Over. Your score was of" ++ show (score gameState))
