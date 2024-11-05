b7089bcb05dd65fcac85a7dce90faddca05122cc
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-11-05 11:07:02 -0500

    Refactored WordBank.hs

--- src/WordBank/WordBank.hs
+++ src/WordBank/WordBank.hs
@@ -2,37 +2,39 @@ module WordBank.WordBank (createWordBank) where
 import Data.List (nub, sort)
 import Data.Char (toLower, isAscii, isLetter)
 
+-- | Create the string containing the word bank.
 createWordBank :: IO String
-createWordBank =
-    makeString .
-    makeNoDuplicateList .
-    readFile $ file
+createWordBank = do
+    fileText <- readFile file
+    return $
+        makeStringFromWordList $
+        textToWordList fileText
 
+
+-- | Where the text file to have the word bank created from is.
 file :: FilePath
 file = "data/text.txt"
 
-makeNoDuplicateList :: IO String -> IO [String]
-makeNoDuplicateList ioString = do
-    string <- ioString
-    return $
-        sort .
+-- | Transform a text to a list of words without duplicate. Only count words with only ascii characters.
+textToWordList :: String -> [String]
+textToWordList = sort .
         filter (not . null) .
         nub . -- remove duplicate elements
         map (filter isLetter . filter isAscii . map toLower) .
-        words $ string
+        words
 
-makeString :: IO [String] -> IO String
-makeString l = do
-    listWordBank <- l
-    return $ listToString listWordBank ""
-
-listToString :: [String] -> String -> String
-listToString (x:xs) str = case x:xs of
-    [x] ->
-        str ++ ", " ++ x
-    _ ->
-        if str /= ""
-        then listToString xs
-            $ str ++ ", " ++ x
-        else listToString xs
-            $ str ++ x
+-- | Make the word list to a string by writing each elements between commas.
+makeStringFromWordList :: [String] -> String
+makeStringFromWordList listWordBank = 
+    wordListToString listWordBank "" where
+        
+        wordListToString :: [String] -> String -> String
+        wordListToString (x:xs) str = 
+            case x:xs of
+                -- if there's only one word remaining in the list
+                [x] ->
+                    str ++ ", " ++ x -- Return the list with the last word
+                _ -> -- if anything else
+                    if str == ""
+                    then wordListToString xs $ str ++ x
+                    else wordListToString xs $ str ++ ", " ++ x
